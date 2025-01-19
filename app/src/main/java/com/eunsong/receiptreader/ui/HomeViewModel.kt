72a5bd0d795776e667bba38900c2645057298da9
Eunsong Cheon <freesky111@naver.com>
2025-01-20 02:59:35 +0900

    Added ocr module

---
+++ app/src/main/java/com/eunsong/receiptreader/ui/HomeViewModel.kt
@@ -0,0 +1,23 @@
+package com.eunsong.receiptreader.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+    private val _state = MutableStateFlow<HomeState>(HomeState.Idle)
+    val state: StateFlow<HomeState> get() = _state
+
+    fun handleIntent(intent: HomeIntent) {
+        when (intent) {
+            is HomeIntent.OpenCamera -> _state.value = HomeState.CameraOpen
+            is HomeIntent.CloseCamera -> _state.value = HomeState.Idle
+            is HomeIntent.CaptureImage -> _state.value = HomeState.ImageCaptured(intent.bitmap)
+            is HomeIntent.ShowError -> _state.value = HomeState.Error(intent.message)
+        }
+    }
+}
+
--- camerax/build.gradle.kts
+++ camerax/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
@@ -7,17 +7,14 @@ plugins {
 }
 
 android {
-    namespace = "com.eunsong.receiptreader"
+    namespace = "com.eunsong.camerax"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.eunsong.receiptreader"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,39 +33,28 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-    implementation(project(":camerax"))
-    project(":camerax")
-    project(":gallery")
-    project(":ocr")
+    // camerax
+//    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
     //hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
-    implementation(libs.androidx.hilt.navigation.compose)
 
     //compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(libs.androidx.ui.graphics)
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
